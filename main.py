a68da02cc51aebbfd2052c4bd0722404685147ba
Alexander Kurchin <80466494+alexander-kurchin@users.noreply.github.com>
2023-07-26 11:45:22 +0300

    Набросал скелет будущей программы

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
-def run():
+def print_header():
+    header = 'Здесь будет красивый заголовок \n\n\n\n'
+    print(header)
+
+
+def input_players_names():
+    pass
+
+
+def show_field(field):
+    pass
+
+
+def show_rules():
+    pass
+
+
+def check_win():
+    pass
+
+
+def start_game():
     print('Здесь будут «Крестики-нолики»')
+
+
+def run():
+    print_header()
+    show_rules()
+    input_players_names()
+    start_game()
 
 
 if __name__ == '__main__':
