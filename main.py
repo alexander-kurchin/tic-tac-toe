ad1637052a8106e1764f6bd943147501141e0fb0
Alexander Kurchin <80466494+alexander-kurchin@users.noreply.github.com>
2023-07-26 12:02:59 +0300

    Добавил логику проверки победителя

--- main.py
+++ main.py
@@ -72,8 +72,15 @@ def show_rules():
 
 
 def check_win(field, current_player):
-    # Заглушка
-    return True
+    winning_lines = ((0, 1, 2),
+                     (3, 4, 5),
+                     (6, 7, 8),
+                     (0, 3, 6),
+                     (1, 4, 7),
+                     (2, 5, 8),
+                     (0, 4, 8),
+                     (2, 4, 6))
+    return any([all([field[position] == current_player for position in line]) for line in winning_lines])
 
 
 def start_game(players):
